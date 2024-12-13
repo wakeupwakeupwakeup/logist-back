5b2fe2bb0cf00d8aec331380db34928b43808383
visa viceversa <79770505+wakeupwakeupwakeup@users.noreply.github.com>
2024-12-13 21:54:52 +0300

    CORS change

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: 'http://localhost:3000',
+        origin: 'https://wakeupwakeupwakeup-logist-front-0d3f.twc1.net',
         methods: 'GET,POST,PUT,DELETE',
         allowedHeaders: 'Content-Type, Authorization',
     });
