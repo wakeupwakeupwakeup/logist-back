3e885e135a45b6732bc1b7c4f29785baac2e5d30
visa viceversa <79770505+wakeupwakeupwakeup@users.noreply.github.com>
2024-12-16 12:32:37 +0300

    DB Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,19 @@ import { ClientsModule } from './clients/clients.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DriversModule } from './driver/drivers.module';
 import { ReportsModule } from './report/reports,module';
+import { config } from 'dotenv';
+
+config();
 
 @Module({
     imports: [
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: process.env.DATABASE_HOST || 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'admin',
-            database: 'logistic',
+            host: process.env.DATABASE_HOST,
+            port: parseInt(process.env.DATABASE_PORT, 10),
+            username: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_DBNAME,
             synchronize: true,
             autoLoadEntities: true,
         }),
