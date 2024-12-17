9ae442a83e58a6d805223d36452ccd475563a454
visa viceversa <79770505+wakeupwakeupwakeup@users.noreply.github.com>
2024-12-17 12:07:36 +0300

    Port and cors fix

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: 'http://wakeupwakeupwakeup-logist-front-0d3f.twc1.net',
+        origin: 'https://wakeupwakeupwakeup-logist-front-0d3f.twc1.net',
         methods: 'GET,POST,PUT,DELETE, PATCH',
         allowedHeaders: 'Content-Type, Authorization',
     });
-    await app.listen(8080);
 }
 bootstrap();
